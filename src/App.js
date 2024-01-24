8f5848360ad5461b75ebe4232378afc9f40b1231
Rajeeb Razak <rajeebrazak786@gmail.com>
2024-01-24 18:52:35 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './Components/Admin/AuthContext';
+import Login from './Components/Login/Login';
+import Home from './Components/Home/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Spare from './Components/Service/Spare';
+import Service from './Components/Service/Service';
+import Gallery from './Components/Service/Gallery';
+import Cusform from './Components/Service/Form';
+import Register from './Components/Login/Register';
+import Cusprice from './Components/Service/Cusprice';
+import Admin from './Components/Admin/Admin';
+import PrivateRoute from './Components/Admin/Privateroute';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/spare" element={<Spare />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/service" element={<Service />} />
+            <Route path="/form" element={<Cusform />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/price" element={<Cusprice />} />
+            <Route path="/admin" element={<PrivateRoute element={<Admin />} />} />
+
+          </Routes>
+        </AuthProvider>
+      </Router>
     </div>
   );
 }
