914f80dd6aabd6a0a8112b3cd4c019645de7500f
Rajeeb Razak <rajeebrazak786@gmail.com>
2024-01-25 11:06:11 +0530

    cards

---
+++ src/Components/Price/Pricelist.js
@@ -0,0 +1,67 @@
+
+export const Pricelist = [
+    {
+      "id": 1,
+      "serviceType": "Engine Oil Change only",
+      "details" : [ "engine oil & oil filter replacement"],
+      "image": "https://media.istockphoto.com/id/624170542/photo/change-oil-of-motorcycle.jpg?s=1024x1024&w=is&k=20&c=t0Ytk2YwWR46iI-jmJKJkBPAsay3FwxFXQsdiuONtnA=",
+      "price": 150
+    },
+    {
+      "id": 2,
+      "serviceType": "Check up service",
+      "details" : ["check up all parts and replacement including washing"],
+      "image": "https://media.istockphoto.com/id/1409875977/photo/car-mechanic-inspects-motorcycle-breakdowns-in-car-repair-shop.jpg?s=1024x1024&w=is&k=20&c=hf-TonCjcYYyeuzArNJwW2UItcTpHx1YJPBvyefUayg=",
+      "price": 1500
+    },
+    {
+      "id": 3,
+      "serviceType": "Brake Inspection and replacements",
+      "details" : ["Brake shoe replacement and lubrication"],
+      "image": "https://media.istockphoto.com/id/960169988/photo/old-motorcycle-drum-brake-pad-in-opened-engine-box.jpg?s=1024x1024&w=is&k=20&c=bYT69uRk8uczJFlgVAK2rTVSsPU9lPxAg8sLFKrlipw=",
+      "price": 600
+    },
+    {
+      "id": 4,
+      "serviceType": "Chain Sprocket Kit replacement",
+      "details" : ["chain sprocket replacement lubrication "],
+      "image": "https://media.istockphoto.com/id/967294794/photo/man-in-gloves-adjusting-chain-on-rear-motorcycle-wheel.jpg?s=1024x1024&w=is&k=20&c=_BxJlO3kpNKUdmYzxvrwOBSFAPpPRjdi9JCyBI0nFcU=",
+      "price": 1200
+    },
+    {
+      "id": 5,
+      "serviceType": "Spark Plug Replacement",
+      "details" : ["spark plug replacement and engine check up"],
+      "image": "https://media.istockphoto.com/id/470062696/photo/motorcycle-engine-maintenance.jpg?s=1024x1024&w=is&k=20&c=L7kr6G4s2Eq9CmfusXiVX2EiZlR0EVuSPEz1zJwunk8=",
+      "price": 300
+    },
+    {
+      "id": 6,
+      "serviceType": "Full Service",
+      "details": ["engine oil", "washing , full checkup and service"],
+      "image": "https://media.istockphoto.com/id/497859268/vector/vector-motorcycle-spares-concept.jpg?s=1024x1024&w=is&k=20&c=Yk175LgueQ-vBMZ9SlIdh5-5CRHqaq4Bkd3LGVo9fdY=",
+      "price": 6000
+    },
+    {
+      "id": 7,
+      "serviceType": "Washing",
+      "details": [" washing and polishing only "],
+      "image": "https://media.istockphoto.com/id/1216187198/photo/the-man-using-microfiber-wipe-for-washing-a-motorcycle.jpg?s=1024x1024&w=is&k=20&c=V3oLJoPjmSnw86nYXU791E4Qq-aGovFpREHtL_Gf6Kg=",
+      "price": 200
+    },
+    {
+      "id": 8,
+      "serviceType": "Engine repair",
+      "details": ["engine oil", "washing, full engine repair"],
+      "image": "https://media.istockphoto.com/id/481678798/photo/male-mechanic-in-a-repair-workshop.jpg?s=1024x1024&w=is&k=20&c=4M0g8nGFGFYTFKlFNR3cIChfhYapOGEHxEISSK9Qmpg=",
+      "price": 8000
+    },
+    {
+      "id": 9,
+      "serviceType": "wiring kit service",
+      "details": [" wiring kit check up and replacement service"],
+      "image": "https://media.istockphoto.com/id/1308591663/photo/repair-of-electricity-on-a-motorcycle.jpg?s=1024x1024&w=is&k=20&c=MG4SnpHZye6eKH1hkre8MQagpytJjxWfpdYOplg32x8=",
+      "price": 2000
+    }
+  ]
+
---
+++ src/Components/Price/Productlist.js
@@ -0,0 +1,78 @@
+ 
+ 
+ export const Productlist = [
+    {
+      "id": 1,
+      "name": "Brake Pads",
+      "image": "https://m.media-amazon.com/images/I/71tzFaUmF5L._SX679_.jpg",
+      "price": 299
+    },
+    {
+      "id": 2,
+      "name": "Chain Sprocket Kit",
+      "image": "https://m.media-amazon.com/images/I/71cpd4JfESL._SL1500_.jpg",
+      "price": 1909
+    },
+    {
+      "id": 3,
+      "name": "Spark Plugs",
+      "image": "https://m.media-amazon.com/images/I/31hej-cMDfL._SX300_SY300_QL70_FMwebp_.jpg",
+      "price": 103
+    },
+    {
+      "id": 4,
+      "name": " Tubeless Tyre Rear",
+      "image": "https://m.media-amazon.com/images/I/614FQZbS8LL._SX679_.jpg",
+      "price": 3500     
+    },
+    {
+      "id": 5,
+      "name": "Rear Shocker Suspension",
+      "image": "https://m.media-amazon.com/images/I/61W+ppA3K7L._SX679_.jpg",
+      "price": 3400
+    },
+    {
+      "id": 6,
+      "name": "Battery",
+      "image": "https://m.media-amazon.com/images/I/61-yCdpsDSL._SX679_.jpg",
+      "price": 2200
+    },
+    {
+      "id": 7,
+      "name": "Engine Oil",
+      "image": "https://m.media-amazon.com/images/I/31CJO1ThiGL.jpg",
+      "price": 1088
+    },
+    {
+      "id": 8,
+      "name": "front shock spring",
+      "image": "https://m.media-amazon.com/images/I/41xRRutn7xL.jpg",
+      "price": 500
+    },
+    {
+      "id": 9,
+      "name": "Air Filter",
+      "image": "https://m.media-amazon.com/images/I/416BCcNOBZL._SX300_SY300_QL70_FMwebp_.jpg",
+      "price": 250
+    },
+    {
+      "id": 10,
+      "name": "Clutch Plate",
+      "image": "https://m.media-amazon.com/images/I/414a59mmWCL._SY300_SX300_QL70_FMwebp_.jpg",
+      "price": 998
+    },
+    {
+     "id" : 11,
+     "name" : "Fuel Tank",
+     "image" : "https://m.media-amazon.com/images/I/71a5+9+XF8L._SL1500_.jpg",
+     "price" : 1000
+
+    },
+    {
+      "id" : 12,
+      "name" : "Head Light Visor",
+      "image" : "https://m.media-amazon.com/images/I/416x05ae0wL.jpg",
+      "price" : 600
+    }
+  ]
+
